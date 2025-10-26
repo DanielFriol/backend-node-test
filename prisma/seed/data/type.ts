0cc5d4b07a89b20f0223bc2543050b28e17ee536
Daniel Friol <danielfriol@gmail.com>
2025-10-26 11:52:08 -0300

    feat: implement many-to-many relationship between Pokemon and types with migration and seeding

---
+++ prisma/seed/data/type.ts
@@ -0,0 +1,47 @@
+export const types = [
+  {
+    name: 'GRASS',
+  },
+  {
+    name: 'FIRE',
+  },
+  {
+    name: 'WATER',
+  },
+  {
+    name: 'BUG',
+  },
+  {
+    name: 'NORMAL',
+  },
+  {
+    name: 'POISON',
+  },
+  {
+    name: 'ELECTRIC',
+  },
+  {
+    name: 'GROUND',
+  },
+  {
+    name: 'FAIRY',
+  },
+  {
+    name: 'FIGHTING',
+  },
+  {
+    name: 'PSYCHIC',
+  },
+  {
+    name: 'ROCK',
+  },
+  {
+    name: 'GHOST',
+  },
+  {
+    name: 'ICE',
+  },
+  {
+    name: 'DRAGON',
+  },
+];
